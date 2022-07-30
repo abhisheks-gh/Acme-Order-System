61b10f0dcb3e3cad1f280dc3d82c26f7849f1685
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-07-30 09:39:17 +0530

    Added TestGoods.java

---
+++ src/com/acmeordersystem/testing/TestGoods.java
@@ -0,0 +1,34 @@
+package com.acmeordersystem.testing;
+
+import com.acmeordersystem.domain.Good;
+import com.acmeordersystem.domain.Liquid;
+import com.acmeordersystem.domain.Solid;
+import com.acmeordersystem.domain.Good.UnitOfMeasureType;
+
+public class TestGoods {
+	public static void main(String[] args) {
+		Liquid glue = new Liquid("Acme Glue", 2334, 4,
+				UnitOfMeasureType.LITER, false, 15, 6);
+		Liquid paint = new Liquid("Acme Invisible Paint", 2490,
+				0.65,
+				UnitOfMeasureType.GALLON, true, 0.70, 12);
+		Solid anvil = new Solid("Acme Anvil", 1668, 0.3,
+				UnitOfMeasureType.CUBIC_METER, false, 5000, 0.5, 0.5);
+		System.out.println(glue);
+		System.out.println(paint);
+		System.out.println(anvil);
+
+		System.out.println("The weight of " + glue + " is " +
+				glue.weight());
+		System.out.println("The weight of " + paint + " is " +
+				paint.weight());
+		System.out.println("The weight of " + anvil + " is " +
+				anvil.weight());
+		Good x = glue;
+		System.out.println("Is " + x + " flammable? " +
+				x.isFlammable());
+		x = paint;
+		System.out.println("Is " + x + " flammable? " +
+				x.isFlammable());
+	}
+}

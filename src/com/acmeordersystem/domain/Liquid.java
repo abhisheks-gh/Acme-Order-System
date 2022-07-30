b525b1e355102e7065c12314c4f69f60b29f32de
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-07-30 09:42:24 +0530

    Update Liquid.java

--- src/com/acmeordersystem/domain/Liquid.java
+++ src/com/acmeordersystem/domain/Liquid.java
@@ -4,7 +4,7 @@ public class Liquid extends Good {
     /** Radius of the liquid container */
     private double radius;
 
-    public Liquid(String name, int modelNumber, double height, unitOfMeasureType unitOfMeasure, boolean flammable,
+    public Liquid(String name, int modelNumber, double height, UnitOfMeasureType unitOfMeasure, boolean flammable,
                   double weightPerUofM, double radius) {
         // Constructor chaining
         super(name, modelNumber, height, unitOfMeasure, flammable, weightPerUofM);

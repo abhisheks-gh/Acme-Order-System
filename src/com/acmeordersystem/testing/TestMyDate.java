d1a066499926db95db854995c9587d9df8c737d4
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-08-09 01:43:50 +0530

    Update TestMyDate.java
    Overrided the equals() method in MyDate class and tested it.

--- src/com/acmeordersystem/testing/TestMyDate.java
+++ src/com/acmeordersystem/testing/TestMyDate.java
@@ -5,18 +5,18 @@ import com.acmeordersystem.utils.MyDate;
 public class TestMyDate{
 
 	public static void main(String[] args) {
-		MyDate date1 = new MyDate((byte)11, (byte)11,1918);
+		MyDate date1 = new MyDate((byte)11, (byte)11, (short)1918);
 
 		MyDate date2 = new MyDate();
 		date2.setDay(11);
 		date2.setMonth(11);
-		date2.setYear(1918);
+		date2.setYear((short)1918);
 
 		MyDate date3 = new MyDate();
-		date3.setDate((byte)4, (byte)21,1968);
+		date3.setDate((byte)4, (byte)21, (short)1968);
 
 		// Non-valid date detection test.
-		date3.setDate((byte)13, (byte)40, -1);
+		date3.setDate((byte)13, (byte)40, (short)-1);
 
 		String str1 = date1.toString();
 		String str2 = date2.toString();
@@ -28,6 +28,14 @@ public class TestMyDate{
 
 		// tests the method leapYears()
 		// MyDate.leapYears();
+
+		MyDate newYear = new MyDate((byte)1, (byte)1, (short)2009);
+		MyDate fiscalStart = new MyDate((byte)1, (byte)1, (short)2009);
+
+		if (newYear.equals(fiscalStart))
+			System.out.println("These two dates are equal");
+		else
+			System.out.println("These two dates are not equal");
 	}
 
 }

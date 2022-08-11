498c86c26ae73cffb46676dba48ecc0faf8e28e0
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-08-11 19:27:24 +0530

    Added Product Interface
    Created Product Interface which both Goods and Service classes can use.

---
+++ src/com/acmeordersystem/domain/Product.java
@@ -0,0 +1,7 @@
+package com.acmeordersystem.domain;
+
+public interface Product {
+    public abstract String getName();
+    public abstract void setName(String n);
+    public abstract String toString();
+}
